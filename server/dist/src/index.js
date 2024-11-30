2ac3fb77cceb4cee79080252b5dd367498380d36
Siddhesh Sule <64964929+siddheshsule@users.noreply.github.com>
2024-11-30 16:30:07 +0100

    Dashboard controller added

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -9,23 +9,22 @@ const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
 const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
-// ROUTE IMPORTS
-// CONFIGURATIONS
+/* ROUTE IMPORTS */
+const dashboardRoutes_1 = __importDefault(require("./routes/dashboardRoutes"));
+/* CONFIGURATIONS */
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, helmet_1.default)());
-app.use(helmet_1.default.crossOriginResourcePolicy({ policy: 'cross-origin' }));
-app.use((0, morgan_1.default)('common'));
+app.use(helmet_1.default.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use((0, morgan_1.default)("common"));
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: false }));
 app.use((0, cors_1.default)());
-// ROUTES
-app.get("/hello", (req, res) => {
-    res.send("Hello World2");
-});
-// SERVER
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+/* ROUTES */
+app.use("/dashboard", dashboardRoutes_1.default); // http://localhost:8000/dashboard
+/* SERVER */
+const port = Number(process.env.PORT) || 3001;
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Server running on port ${port}`);
 });
