3a896e0564f00c91b495f3b83a2253aac0abb049
Siddhesh Sule <64964929+siddheshsule@users.noreply.github.com>
2024-12-07 10:15:52 +0100

    Card purchase aummary added

--- client/src/app/dashboard/CardPurchaseSummary.tsx
+++ client/src/app/dashboard/CardPurchaseSummary.tsx
@@ -1,8 +1,43 @@
+import { useGetDashboardMetricsQuery } from '@/state/api';
+import numeral from 'numeral';
 import React from 'react'
 
 const CardPurchaseSummary = () => {
+  const { data, isLoading } = useGetDashboardMetricsQuery();
+  const purchaseData = data?.purchaseSummary || [];
+
+  const lastDataPoint = purchaseData[purchaseData.length - 1] || null;
+
   return (
-    <div className='row-span-2 xl:row-span-3 col-span-2 xl:col-span-1 bg-gray-500'>CardPurchaseSummary</div>
+    <div className='flex bg-white justify-between flex-col row-span-2 xl:row-span-3 col-span-2 xl:col-span-1 shadow-md rounded-xl'>
+      {isLoading ? (
+        <div >Loading...</div>
+      ):(<>
+      {/* Header */}
+      <div>
+        <h2 className='text-lg font-semibold mb-2 px-7 pt-5'>
+          Purchase Summary
+        </h2>
+      </div>
+
+      {/* Body */}
+      <div>
+        <div className='mb-4 mt-7 px-7'>
+          <p className='text-xs text-gray-400'>Purchased</p>
+          <div className='flex items-center'>
+            <p className='text-2xl font-bold'>
+              {lastDataPoint 
+              ? numeral(lastDataPoint.totalPurchased).format('0.0a')
+              : "0"}
+            </p>
+          </div>
+          {lastDataPoint && (
+            <p ></p>
+          )}
+        </div>
+      </div>
+      </>)}
+    </div>
   )
 }
 
